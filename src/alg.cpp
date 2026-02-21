7d34def9fb909046241482f142c52ae7ee7cab53
timchernow <www.tiimchernov@mail.ru>
2026-02-22 00:03:48 +0300

    second prime

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,13 +3,14 @@
 #include <cstdint>
 #include "alg.h"
 
-bool checkPrime(uint64_t value) {
+bool checkPrime(uint64_t value) 
+{
     if (value <= 1) return false;
     if (value == 2) return true;
     if (value % 2 == 0) return false;
     
     uint64_t limit = sqrt(value);
-    for(uint64_t i = 3; i <= limit; i += 2) {
+    for (uint64_t i = 3; i <= limit; i += 2) {
         if (value % i == 0) {
             return false;
         }
@@ -17,7 +18,8 @@ bool checkPrime(uint64_t value) {
     return true;
 }
 
-uint64_t nPrime(uint64_t n) {
+uint64_t nPrime(uint64_t n) 
+{
     if (n == 0) return 0;
     uint64_t counter = 0;
     uint64_t number = 1;
